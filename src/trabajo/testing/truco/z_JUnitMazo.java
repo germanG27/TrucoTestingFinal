acbe05e37afd3b7e3d9ddd28c4d8a3138c00d7b2
rodrigonievaub <rodrigo.nieva@comunidad.ub.edu.ar>
2018-02-16 17:54:06 -0300

    Se agrego Bug Report Habitica y una modificacion.

--- src/trabajo/testing/truco/z_JUnitMazo.java
+++ src/trabajo/testing/truco/z_JUnitMazo.java
@@ -11,11 +11,23 @@ import org.junit.Test;
 
 public class z_JUnitMazo {
 
+	Carta c1;
+	Carta c2;
+	Carta c3;
+	Carta c4;
 	private ArrayList<Carta> mazo;
-	
+	private ArrayList<Carta> mazo1;
+	private static final Integer numero = 100;
+	private static final Integer retirarCarta = 2;
+	Mazo m2;
 	@Before
 	public void setUp() throws Exception {
-	
+		c1 = new Carta(1, Palo.Basto);
+		c2 = new Carta(2, Palo.Oro);
+		c3 = new Carta(4, Palo.Espada);
+		c4 = new Carta(2,Palo.Oro);
+		mazo = new ArrayList<Carta>();
+		mazo1 = new ArrayList<Carta>();
 	}
 
 	@After
@@ -23,13 +35,29 @@ public class z_JUnitMazo {
 	}
 
 	@Test
-	public void repartirCartas() {
-		Carta c1 = new Carta(1, Palo.Basto);
-		mazo.add(c1);
-		Jugador a = new Jugador("a");
-		a.getCartasUser().add(mazo.get(0));
-		assertEquals(12, mazo.retirarCarta(39, mazo.getMazo()).getNumero().intValue());
-		
+	public void agregarCartas() {
+	mazo.add(c1);
+		assertEquals(mazo.get(0), c1);
 	}
-
+	
+	@Test
+	public void setearPuntos(){
+		c1.setPuntaje(100);
+		assertEquals(c1.getPuntaje(), numero);
+	}
+	
+	@Test
+	public void retirarCarta(){
+		mazo1.add(c1);
+		mazo1.add(c2);
+		mazo1.add(c3);
+		mazo1.add(c4);
+		if(mazo1.size() > retirarCarta){
+			mazo1.remove(2);
+		}
+		else{
+			
+		}
+		assertEquals(mazo1.get(1).toString(), mazo1.get(2).toString());
+	}
 }
